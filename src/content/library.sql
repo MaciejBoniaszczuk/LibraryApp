90f3bc618d57b199c8de07cd45f91f6ffb4d80fd
LukaszKudd <lukasz.kuderewski@gmail.com>
2019-12-07 20:32:22 +0200

    Checking if a user exist

---
+++ src/content/library.sql
@@ -0,0 +1,176 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 21 Lis 2019, 22:24
+-- Wersja serwera: 10.4.8-MariaDB
+-- Wersja PHP: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `library`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `login` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `password` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Zrzut danych tabeli `admin`
+--
+
+INSERT INTO `admin` (`id`, `login`, `password`) VALUES
+(1, 'lehu', 'elooo22');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `books`
+--
+
+CREATE TABLE `books` (
+  `idBooks` int(11) NOT NULL,
+  `Title` text COLLATE utf8_polish_ci NOT NULL,
+  `First Name` text COLLATE utf8_polish_ci NOT NULL,
+  `Last Name` text COLLATE utf8_polish_ci NOT NULL,
+  `Publisher` text COLLATE utf8_polish_ci NOT NULL,
+  `Year` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `books`
+--
+
+INSERT INTO `books` (`idBooks`, `Title`, `First Name`, `Last Name`, `Publisher`, `Year`) VALUES
+(2, 'PHP : 101 praktycznych skryptów', 'Marcin', 'Lis', 'Helion', 2007),
+(3, 'PHP', 'Julie', 'C. Meloni', 'Wydawnictwo RM', 2001),
+(4, 'PHP : praktyczne projekty', 'Włodzimierz', 'Gajda', 'Helion', 2009),
+(5, 'PHP i MySQL : 101 porad', 'Łukasz', 'Sosna', 'Mikom', 2005);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `borred`
+--
+
+CREATE TABLE `borred` (
+  `idBorred` int(11) NOT NULL,
+  `idClients` int(11) NOT NULL,
+  `idBooks` int(11) NOT NULL,
+  `Date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Zrzut danych tabeli `borred`
+--
+
+INSERT INTO `borred` (`idBorred`, `idClients`, `idBooks`, `Date`) VALUES
+(1, 2, 1, '2019-05-26'),
+(2, 3, 4, '2019-07-15'),
+(3, 2, 1, '2019-10-20'),
+(4, 4, 4, '2019-03-13');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `clients`
+--
+
+CREATE TABLE `clients` (
+  `idClients` int(11) NOT NULL,
+  `First Name` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `Last Name` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `Index Number` int(11) NOT NULL,
+  `login` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `password` text COLLATE utf8mb4_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Zrzut danych tabeli `clients`
+--
+
+INSERT INTO `clients` (`idClients`, `First Name`, `Last Name`, `Index Number`, `login`, `password`) VALUES
+(1, 'Katarzyna', 'Andruszewicz', 145698, 'kaa', '123'),
+(2, 'Anna', 'Kowal', 628465, 'anni', 'iksde'),
+(3, 'Krzysztof', 'Kopeć', 456458, 'kopi', 'xxixx'),
+(4, 'Artur', 'Romanowicz', 768249, 'romek', 'rOman');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`idBooks`);
+
+--
+-- Indeksy dla tabeli `borred`
+--
+ALTER TABLE `borred`
+  ADD PRIMARY KEY (`idBorred`);
+
+--
+-- Indeksy dla tabeli `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`idClients`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `books`
+--
+ALTER TABLE `books`
+  MODIFY `idBooks` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT dla tabeli `borred`
+--
+ALTER TABLE `borred`
+  MODIFY `idBorred` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT dla tabeli `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `idClients` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/content/log.php
+++ src/content/log.php
@@ -6,25 +6,29 @@ $connection = @new mysqli($host, $db_user, $db_password, $db_name);
 
 if ($connection->connect_errno!=0)
 {
-  echo "Error: ".$connection->connect_errno . "Opis: ".$connection->connect_error;
+  echo "Error: ".$connection->connect_errno;
 }
-else
-{
+else {
   $login = $_POST['userName'];
   $password = $_POST['password'];
 
-  echo "Działa";
+  $sql = "SELECT * FROM clients WHERE login='$login' AND password='$password'";
 
+
+  if ($result = @$connection->query($sql))
+  {
+    $how_many_users = $result->num_rows;
+    if ($how_many_users>0)
+    {
+      $row = $result->fetch_assoc();
+      $user = $row['login'];
+
+      $result->free_result();
+    } else {
+      
+    }
+  }
   $connection->close();
 }
-
-
-
-// echo $login."<br />";
-// echo $password;
-
-
-
-
 
 ?>

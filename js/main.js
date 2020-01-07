07db77007d827ea3693e29ab4836c11664598e3b
Maciek Boniaszczuk <maciek6001@gmail.com>
2020-01-07 21:35:54 +0100

    feat: simple api fetch added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 // place your code below
+const testowy_div = document.querySelector('.testowy_div')
+
 
 fetch('http://localhost:3000/products/')
     .then(resp => {
         return resp.json();
     })
     .then(data => {
-        console.log(data);
+        const repos = data;
+        for (var i = 0; i < 4; i++) {
+            testowy_div.innerHTML += `<div>${repos.products[i].name}</div>`
+        }
+
     })
 
 

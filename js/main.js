e03e82c109e3e9a2cc78236dfd079ea9415090e0
tronsez <mateuszulenberg@gmail.com>
2020-01-08 15:12:44 +0100

    feat: new look

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // place your code below
-const testowy_div = document.querySelector('.testowy_div')
-
+const wszystkie_ksiazki = document.querySelector('.wszystkie_ksiazki')
 
 fetch('http://localhost:3000/products/')
     .then(resp => {
@@ -8,11 +7,37 @@ fetch('http://localhost:3000/products/')
     })
     .then(data => {
         const repos = data;
-        for (var i = 0; i < 4; i++) {
-            testowy_div.innerHTML += `<div>${repos.products[i].name}</div>`
+        for (var i = 0; i < 5; i++) {
+            wszystkie_ksiazki.innerHTML += `<div><img src="assets/img/logoblue.png" width="20" height="20" hspace="5">${repos.products[i].name}</div>`
         }
 
     })
 
+//console.log(`Hello world!`);
 
-console.log(`Hello world!`);
+const wolne_ksiazki = document.querySelector('.wolne_ksiazki')
+fetch('http://localhost:3000/products/')
+    .then(resp => {
+        return resp.json();
+    })
+    .then(data => {
+        const repos = data;
+        for (var i = 0; i < 5; i++) {
+            wolne_ksiazki.innerHTML += `<div><img src="assets/img/logogreen.png" width="20" height="20" hspace="5">${repos.products[i].name}</div>`
+        }
+
+    })
+
+const zalogowany = document.querySelector('.zalogowany')
+fetch('http://localhost:3000/products/')
+    .then(resp => {
+        return resp.json();
+    })
+    .then(data => {
+        const repos = data;
+        for (var i = 0; i < 5; i++) {
+            zalogowany.innerHTML += `<div><img src="assets/img/logogreen.png" width="20" height="20" hspace="5">${repos.products[i].name}</div>`
+        }
+
+    })
+

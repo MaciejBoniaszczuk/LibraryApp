43235d544eac9207f8455504d1ee945016b661a6
Maciek Boniaszczuk <maciek6001@gmail.com>
2020-01-09 18:00:38 +0100

    add css to dashboard and spis

--- public/js/main.js
+++ public/js/main.js
@@ -7,8 +7,8 @@ fetch('http://localhost:3000/products/')
     })
     .then(data => {
         const repos = data;
-        for (var i = 0; i < 6; i++) {
-            wszystkie_ksiazki.innerHTML += `<div><img src="http://localhost:3000/myFiles/assets/img/logoblue.png" width="20" height="20" hspace="5">${repos.products[i].name}</div>`
+        for (var i = 0; i < repos.count; i++) {
+            wszystkie_ksiazki.innerHTML += `<div class="ksiazki_wszystkie"><img src="http://localhost:3000/myFiles/assets/img/logoblue.png" width="20" height="20" hspace="5">${repos.products[i].name}</div>`
         }
 
     })
@@ -22,21 +22,21 @@ fetch('http://localhost:3000/products/')
     })
     .then(data => {
         const repos = data;
-        for (var i = 0; i < 6; i++) {
-            wolne_ksiazki.innerHTML += `<div><img src="http://localhost:3000/myFiles/assets/img/logogreen.png" width="20" height="20" hspace="5">${repos.products[i].name}</div>`
+        for (var i = 0; i < repos.count; i++) {
+            wolne_ksiazki.innerHTML += `<div class="ksiazki_wszystkie"><img src="http://localhost:3000/myFiles/assets/img/logogreen.png" width="20" height="20" hspace="5">${repos.products[i].name}</div>`
         }
 
     })
 
-const zalogowany = document.querySelector('.zalogowany')
-fetch('http://localhost:3000/products/')
-    .then(resp => {
-        return resp.json();
-    })
-    .then(data => {
-        const repos = data;
-        for (var i = 0; i < 6; i++) {
-            zalogowany.innerHTML += `<div><img src="http://localhost:3000/myFiles/assets/img/logogreen.png" width="20" height="20" hspace="5">${repos.products[i].name}</div>`
-        }
+// const zalogowany = document.querySelector('.zalogowany')
+// fetch('http://localhost:3000/products/')
+//     .then(resp => {
+//         return resp.json();
+//     })
+//     .then(data => {
+//         const repos = data;
+//         for (var i = 0; i < 6; i++) {
+//             zalogowany.innerHTML += `<div><img src="http://localhost:3000/myFiles/assets/img/logogreen.png" width="20" height="20" hspace="5">${repos.products[i].name}</div>`
+//         }
 
-    })
+//     })

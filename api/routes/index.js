edea769225f7c3935597a58a467b519566ceac2e
tronsez <mateuszulenberg@gmail.com>
2020-01-10 00:22:20 +0100

    feat: addBook.ejs + style

--- api/routes/index.js
+++ api/routes/index.js
@@ -9,5 +9,6 @@ router.get('/mailexists', (req, res) => res.render('mailexists'));
 router.get('/dashboard', (req, res) => res.render('dashboard'));
 router.get('/authfailed', (req, res) => res.render('authfailed'));
 router.get('/spis', (req, res) => res.render('spis'));
+router.get('/addBook', (req, res) => res.render('addBook'));
 
 module.exports = router;
